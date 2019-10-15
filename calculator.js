8d7f41cd78248154f1f4d919b3718f72ff1403cd
Devin Mauck <jmauck86@gmail.com>
2019-10-14 21:23:50 -0400

    Functioning and CSS

---
+++ calculator.js
@@ -0,0 +1,43 @@
+// Button on Click
+function btnClick(a) {
+    calcForm.display.value += a;
+}
+
+// Button on Key Press
+let display = document.getElementsByClassName('display');
+document.body.onkeypress = function numPress() {
+    // Select keys 0-9
+    for (var i = 0; i < 10; i++) {
+        if (event.keyCode == i + 48) calcForm.display.value += i;
+    }
+};
+
+
+//Equals Function
+function equalsTo() {
+    calcForm.display.value = eval(calcForm.display.value);
+}
+// Return key onkeypress
+document.onkeypress = enter;
+function enter(e) {
+    if (e.which == 13) { equalsTo(); }
+}
+
+
+// Reset/All Clear Function
+function clearForm() {
+    calcForm.display.value = "";
+}
+// C Key to Clear
+
+
+// Backspace Function
+function backspace() {
+    var preValue = calcForm.display.value;
+    calcForm.display.value = preValue.substr(0, preValue.length - 1);
+};
+
+function keyPress() {
+    var keys = ['+', '-', '*', '/']
+
+}
